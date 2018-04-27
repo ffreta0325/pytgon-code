be614d3ce8efcc807e41dce0a2401a3d24661f34
Wei An <ffreta0325@gmail.com>
2018-04-27 17:27:29 +0800

    hello

---
+++ hello.py
@@ -0,0 +1,32 @@
+print("Hello word")
+print("Hello","World")
+print("Hello","world",sep="-")
+print("Hello","World",sep="$",end="&")
+print("ABC")
+
+print(9//2)
+print(9%5)
+print(3**4)
+
+a=1
+b=2
+print(a+b)
+
+str1="I am learning programing"
+print(str1)
+print(len(str1))
+
+print(str1[2])
+print(str1[5:13])
+print(str1[5:])
+print(str1[:13])
+print(str1[:])
+print(str1[::-1])
+
+print("{0} {1}".format(100, 200))
+print("nim1:{0} num2: {1}".format(a, b))
+print("{0:10} {1:10}".format(200, 300))
+
+print("{0:<7}{1:<7}{2:<7}".format(12345, 456, 9998))
+print("{0:>7}{1:>7}{2:>7}".format(12345, 456, 9998))
+
